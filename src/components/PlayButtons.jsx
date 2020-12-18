a7b6e62ce26f182dd08b7c30d8f6c2a030bbe617
Ehugo2000 <erik.hgsn@gmail.com>
2020-12-18 16:19:38 +0100

    Installing Semantic Ui and adding styling to play buttons.

--- src/components/PlayButtons.jsx
+++ src/components/PlayButtons.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import { Button, Icon } from "semantic-ui-react";
 
 const PlayButtons = (props) => {
   return (
     <>
+      <Button
+        animated="vertical"
+        data-cy="rock-btn"
+        onClick={() => props.callBack("rock")}
+      >
+        <Button.Content visible>Play Rock</Button.Content>
+        <Button.Content hidden>
+          <Icon name="hand rock" />
+        </Button.Content>
+      </Button>
+
+      <Button
+        animated="vertical"
+        data-cy="paper-btn"
+        onClick={() => props.callBack("paper")}
+      >
+        <Button.Content visible>Play Paper</Button.Content>
+        <Button.Content hidden>
+          <Icon name="hand paper" />
+        </Button.Content>
+      </Button>
+
+      <Button
+        animated="vertical"
+        data-cy="scissor-btn"
+        onClick={() => props.callBack("scissor")}
+      >
+        <Button.Content visible>Play Scissor</Button.Content>
+        <Button.Content hidden>
+          <Icon name="hand scissors" />
+        </Button.Content>
+      </Button>
+      {/*     
       <button data-cy="rock-btn"
       onClick={() => props.callBack("rock")}
       >Play Rock</button>
@@ -11,7 +45,7 @@ const PlayButtons = (props) => {
       >Play Paper</button>
       <button data-cy="scissor-btn"
        onClick={() => props.callBack("scissor")}
-       >Play Scissor</button>
+       >Play Scissor</button> */}
     </>
   );
 };
