ca686a96b3f5d5ed3a7a623dd719c1572feccf88
Ehugo2000 <erik.hgsn@gmail.com>
2020-12-18 15:25:52 +0100

    Rewrite test to work with PlayButtons

--- cypress/integration/userCanSeeResult.feature.js
+++ cypress/integration/userCanSeeResult.feature.js
@@ -3,7 +3,10 @@ describe("User can see result", () => {
     cy.visit("/");
   });
 
+
+
   it("displayed in ResultDisplay component", () => {
+    cy.get('[data-cy="rock-btn"]').click()
     cy.get('[data-cy="display-user-choice"]').should("contain", "You chose rock");
     cy.get('[data-cy="display-computer-choice"]').should("contain", "Computer chose paper");
 
