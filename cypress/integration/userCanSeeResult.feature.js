6cb6dc2031b3a37812e8b00a7a4ee78cfd702da2
Ehugo2000 <erik.hgsn@gmail.com>
2020-12-18 16:00:39 +0100

    Adding getComputerChoice and randomize computer choice

--- cypress/integration/userCanSeeResult.feature.js
+++ cypress/integration/userCanSeeResult.feature.js
@@ -3,13 +3,25 @@ describe("User can see result", () => {
     cy.visit("/");
   });
 
-
-
   it("displayed in ResultDisplay component", () => {
-    cy.get('[data-cy="rock-btn"]').click()
-    cy.get('[data-cy="display-user-choice"]').should("contain", "You chose rock");
-    cy.get('[data-cy="display-computer-choice"]').should("contain", "Computer chose paper");
-
-    
+    cy.get('[data-cy="rock-btn"]').click();
+    cy.get('[data-cy="display-user-choice"]').should(
+      "contain",
+      "You chose rock"
+    );
+    cy.get('[data-cy="paper-btn"]').click();
+    cy.get('[data-cy="display-user-choice"]').should(
+      "contain",
+      "You chose paper"
+    );
+    cy.get('[data-cy="scissor-btn"]').click();
+    cy.get('[data-cy="display-user-choice"]').should(
+      "contain",
+      "You chose scissor"
+    );
+    cy.get('[data-cy="display-computer-choice"]').should(
+      "contain",
+      "Computer chose"
+    );
   });
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import PlayButtons from "./components/PlayButtons";
 import DisplayResults from "./components/DisplayResults";
 
 export const App = () => {
-  const [userChoice, setUserChoice] = useState()
+  const [userChoice, setUserChoice] = useState();
+  const [computerChoice, setComputerChoice] = useState();
+
+  const callBack = (buttonUserChoice) => {
+    setUserChoice(buttonUserChoice);
+    setComputerChoice(getComputerChoice());
+  };
+
+  const getComputerChoice = () => {
+    const random = Math.random();
+    if (random < 0.35) {
+      return "rock";
+    } else if (random <= 0.65) {
+      return "paper";
+    } else {
+      return "scissor";
+    }
+  };
 
   return (
     <>
-      <Header/>
-      <PlayButtons callBack={(buttonUserChoice) => setUserChoice(buttonUserChoice)}/>
-      <DisplayResults myChoice={userChoice} computerChoice={"paper"}/>
+      <Header />
+      <PlayButtons callBack={callBack} />
+      <DisplayResults myChoice={userChoice} computerChoice={computerChoice} />
     </>
   );
 };
