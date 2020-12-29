87d780c19d05dde0c223da10108c02bb464ff782
Ehugo2000 <erik.hgsn@gmail.com>
2020-12-29 15:09:53 +0100

    Fixing error in test

--- cypress/integration/userCanSeeGameIndexPage.feature.js
+++ cypress/integration/userCanSeeGameIndexPage.feature.js
@@ -5,7 +5,7 @@ describe("User can see welcome header and play-buttons", () => {
   });
 
   it("when user visit the application", () => {
-    cy.get('[data-cy="rps-header"]').should("contain", "Welcome to RPS game!");
+    cy.get('[data-cy="rps-header"]').should("contain", "Rock Paper Scissor");
     cy.get('[data-cy="rock-btn"]').should("contain", "Play Rock");
     cy.get('[data-cy="paper-btn"]').should("contain", "Play Paper");
     cy.get('[data-cy="scissor-btn"]').should("contain", "Play Scissor");
